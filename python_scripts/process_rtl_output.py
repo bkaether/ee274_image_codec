fixed negative sign bug

python_scripts/process_rtl_output.py
@@ -9,7 +9,7 @@ shift_file_path = "../memfiles/quantization.mem"
 fractional_bits = 0
 first_block_coeffs = mem_to_ndarray(block1_coeffs_file_path, fractional_bits, 8, 8)
 full_image_coeffs = mem_to_ndarray(full_coeffs_file_path, fractional_bits, 480, 640)
-first_block_dct = mem_to_ndarray(dct_out_file_path, fractional_bits, 8, 8)
+first_block_dct = mem_to_ndarray(dct_out_file_path, 32, 8, 8)
 shift_values = mem_2d_to_ndarray(shift_file_path, 8, 8)
 np.set_printoptions(suppress=True, precision=4)
 image = cv2.imread('../image_data/raw/river.jpg', cv2.IMREAD_GRAYSCALE)

python_scripts/utils.py
@@ -138,7 +138,7 @@ def generate_fixed_cosine_mem(filename, blocksize=8):
             row_str = []
             for j in range(blocksize):
                 cosine_vals_float[i][j] = np.cos((2 * i + 1) * j * np.pi / (2 * 8))
-                fixed, representation = float_to_fixed_point(cosine_vals_float[i][j], 1, 8)
+                fixed, representation = float_to_fixed_point(cosine_vals_float[i][j], 2, 8)
                 cosine_vals_fixed[i][j] = fixed
                 row_str.append(representation)
             # Write the row to the file